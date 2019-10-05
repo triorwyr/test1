cb04d9a9c683762e9b8e625b9f936578aa54e692
triorwyr <56180562+triorwyr@users.noreply.github.com>
2019-10-05 10:30:23 +0800

    第二次修改文件
    111

---
+++ my new file.cpp
@@ -0,0 +1,21 @@
+include<iostream>
+using namespace std;
+class myhellowworld
+{
+public:
+     myhelloworld()
+     {
+       this->hellow="Hellow world!";   
+     }
+     void show()
+     {
+        cout<<this->hellow;
+     }
+     string hellow;
+}
+int main(void)
+{
+   myhelloworld m1;
+   m1.show();
+   return 0;
+}
